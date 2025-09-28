2f65fca6a5d1e4e2a440ea3cae6c11bac76b3436
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-09-28 18:09:25 +0530

    feat(upload): implement file upload to server

--- client/app/components/FileUpload.tsx
+++ client/app/components/FileUpload.tsx
@@ -8,10 +8,23 @@ const FileUpload = () => {
     const el=document.createElement('input')
     el.setAttribute('type', 'file')
     el.setAttribute('accept','application/pdf')
-    el.addEventListener('change', (ev)=>{
+    el.addEventListener('change', async (ev)=>{
       if(el.files && el.files.length>0){
         const file=el.files.item(0);
         // console.log(el.files);
+
+        if(file){
+          const formData=new FormData()
+          formData.append('pdf', file)
+
+          await fetch('http://localhost:3001/upload/pdf',{
+            method: 'POST',
+            body: formData
+          })
+          console.log("File uploaded successfully")
+
+        }
+
       }
     })
     el.click();
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import multer from 'multer'
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, `${uniqueSuffix}-${file.originalname}`)
+  }
+})
+
+
+const upload=multer({storage: storage})
+
+const app=express()
+app.use(cors())
+
+app.get('/', (req, res)=>{
+    return res.json({status:"Perfect"})
+})
+
+app.post('/upload/pdf', upload.single('pdf'), (req, res)=>{
+    return res.json({message: 'uploaded'})
+})
+app.listen(3001, ()=>{
+    console.log(`Server is running on port: 3001`)
+})
